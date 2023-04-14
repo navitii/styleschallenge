07fe59043d89455e605443feff9e54a9ec531566
navitii <aeduardobri@gmail.com>
2023-04-14 17:47:05 -0400

    Se agrega el proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        {/* <div className='bx bx-menu' id="menu-icon"></div> */}
+        <a href="#" className='logo'><img src='chicks-logo-large.png'/></a>
+
+        
+        <ul className="navbar">
+          <li><a href="#">CURRENCY</a></li>
+          <li><a href="#">ITEMS</a></li>
+          <li><a href="#">ACCOUNTS</a></li>
+          <li><a href="#">SERVICES</a></li>
+          <li><a href="#">SWAP</a></li>
+          <li><a href="#">SELL</a></li>
+        </ul>
+
+        <div className='main'>
+        <a href='#' className='user'>USD</a>
+          <div className='sign'>
+            <button href='#' className='user2'>SIGN IN < i className="ri-user-fill"></i></button>
+          </div>
+        </div>
       </header>
     </div>
   );
